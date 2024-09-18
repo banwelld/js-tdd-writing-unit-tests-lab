3541e5324453fc83617d0eeb8ab0d314d7f86519
Dave Banwell <dave@davebanwell.com>
2024-09-17 22:04:05 -0400

    passes tests

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,16 @@
-// Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it('Is true for words spelled the same forwards and backwards', () => {
+        expect(isPalindrome("racecar")).toBe(true);
+    });
+    it('Is false for words spelled differently forwards and backwards', () => {
+        expect(isPalindrome("hello")).toBe(false);
+    });
+    it('Disregards character case', () => {
+        expect(isPalindrome("Racecar")).toBe(true);
+    });
+    it('Returns false for an empty string', () => {
+        expect(isPalindrome("")).toBe(false);
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,5 @@
-// Your code here
+export function isPalindrome(word) {
+    if (!word) return false;
+    const wordArr = word.toLowerCase().split('');
+    return wordArr.join('') === wordArr.reverse().join('');
+};
