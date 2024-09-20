bb898a70ab616ab93db62cf1bcb7976f4a5573db
Dave Banwell <dave@davebanwell.com>
2024-09-19 20:36:31 -0400

    Does everything

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,16 +1,23 @@
 import { isPalindrome } from "../utils";
 
 describe("isPalindrome", () => {
-    it('Is true for words spelled the same forwards and backwards', () => {
+    it('Returns true for words spelled the same forwards and backwards', () => {
         expect(isPalindrome("racecar")).toBe(true);
     });
-    it('Is false for words spelled differently forwards and backwards', () => {
+    it('Returns false for words spelled differently forwards and backwards', () => {
         expect(isPalindrome("hello")).toBe(false);
-    });
-    it('Disregards character case', () => {
-        expect(isPalindrome("Racecar")).toBe(true);
     });
     it('Returns false for an empty string', () => {
         expect(isPalindrome("")).toBe(false);
+    });
+    it('Doesn\'t discriminate between uppercase and lowercase', () => {
+        expect(isPalindrome("RaCecar")).toBe(true);
+    });
+    it('Throws an error if the string contains non-alphabetic characters', () => {
+        expect(() => isPalindrome("hello, world!")).toThrowError();
+    });
+    it('Throws an error if the argument is not a string', () => {
+        const dadArray = ["d","a","d"];
+        expect(() => isPalindrome(dadArray)).toThrowError();
     });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,7 @@
 export function isPalindrome(word) {
     if (!word) return false;
+    if (typeof word != 'string') throw new Error('The submission must be of type \'string\'');
+    if (!/^[A-Za-z]+$/.test(word)) throw new Error('The submission must not contain spaces, numbers, or special characters')
     const wordArr = word.toLowerCase().split('');
     return wordArr.join('') === wordArr.reverse().join('');
-};
+  };
